443a75c9a96da07996676ceea72e5d56049baa42
Masthead <alexeev.tolik2015@yandex.ru>
2023-03-08 20:24:15 +0300

    js-tasks

--- 2.0 JavaScript Basics/2.11/taskOne.js
+++
@@ -1,5 +0,0 @@
-// Что выведет код ниже?
-
-alert(null || 2 || undefined)
-
-// код выведет число 2, тк он совпадает под сравнение true (null и undefined false)
--- 2.0 JavaScript Basics/2.6/taskOne.js
+++
@@ -1,6 +0,0 @@
-// Создайте страницу, которая спрашивает имя у пользователя и выводит его.
-
-let userName = prompt('Как ваше имя?')
-
-alert(userName)
-console.log(userName)
